8a14aaefb77ced1182c0ff27e8ad94334a277e36
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-08-14 22:18:08 -0700

    Adding react-router-dom

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,7 @@ import GalleryItem from './GalleryItem'
 
 const Gallery = function(props) {
     let galleryItems = props.songs.map ((song, i ) => {
-        return <GalleryItem songTitle={song.trackName} artist={song.artistName} />
+        return <GalleryItem key={i} item= {song}  />
     })
     return (
         <div>
